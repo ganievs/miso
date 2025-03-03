2eff6b1f66ca247a6334c1e3b3f75e7554b4070a
Shamil Ganiev <ganiev@pm.me>
2025-03-03 18:30:04 +0200

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
@@ -5,6 +6,9 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  test: {
+    env: "happy-dom",
+  },
   server: {
     proxy: {
       '/api': {
